205e919e1c95dc8b717763b9106d465c01d1eda7
tnwaps <65656829+tnwaps@users.noreply.github.com>
2020-07-17 00:00:19 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"please read the <a href='https://t.me/c/{channel_id}/{message_id}'>Pinned Message</a>",
+        f"please read the <a href='https://t.me/torrenstleech/40'>Pinned Message</a>",
         quote=True
     )
 
@@ -48,12 +48,12 @@ async def rename_message_f(client, message):
     inline_keyboard.append([
         pyrogram.InlineKeyboardButton(
             text="read this?",
-            url="https://t.me/keralagram/698909"
+            url="https://t.me/torrenstleech/40"
         )
     ])
     reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
     await message.reply_text(
-        "please use @renamebot",
+        "please use @tnvipme",
         quote=True,
         reply_markup=reply_markup
     )
